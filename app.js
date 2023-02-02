fc964ebbed69b412b9c247d5403c3e3ddeb54985
leideng <3412829742@qq.com>
2023-02-02 16:06:50 +0800

    获取用户信息

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch (options) {
+    // Do something initial when launch.
+    console.log(options);
+  },
+  onShow (options) {
+    // Do something when show.
+    // console.log(options);
+    // // 判断小程序的进入场景
+    // switch(options.scene){
+    //   case 1001:
+    //     break;
+    //   case 1005:
+    //     break;
+    // }
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    // 获取用户信息，获取到之后，将用户信息传递给服务器
+    wx.getUserInfo({
+      success:(res)=>{
+        console.log(res);
       }
     })
   },
-  globalData: {
-    userInfo: null
+  onHide () {
+    // Do something when hide.
+  },
+  onError (msg) {
+    console.log(msg)
+  },
+  // 可全局调用的数据
+  globalData:{
+    name:'huanghuang',
+    age:18
   }
 })
